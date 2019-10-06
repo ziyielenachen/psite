5de297ed69982a6606508681cb099592ce70e40b
Elena Chen <elena.chen@berkeley.edu>
2019-10-06 13:32:16 -0700

    Updates

---
+++ precache-manifest.0685d377fd9489dabfb9360e212fee5a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025349b30d558102475c3e6132b13398",
+    "url": "/psite/index.html"
+  },
+  {
+    "revision": "dbe30efae2af7338de8a",
+    "url": "/psite/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "95f977f5117320ac1455",
+    "url": "/psite/static/css/main.fbb8617b.chunk.css"
+  },
+  {
+    "revision": "dbe30efae2af7338de8a",
+    "url": "/psite/static/js/2.9e43d600.chunk.js"
+  },
+  {
+    "revision": "95f977f5117320ac1455",
+    "url": "/psite/static/js/main.5793ff4b.chunk.js"
+  },
+  {
+    "revision": "eef72582d46618e1950c",
+    "url": "/psite/static/js/runtime-main.79f2eed5.js"
+  },
+  {
+    "revision": "44ea071862f7cc2a91990e5c18f3bba9",
+    "url": "/psite/static/media/data8.44ea0718.png"
+  },
+  {
+    "revision": "71335085f985e4e2bb528cec739c1283",
+    "url": "/psite/static/media/dpr.71335085.png"
+  },
+  {
+    "revision": "7307d282694ade0c49307ba6c949ea28",
+    "url": "/psite/static/media/linkedin.7307d282.png"
+  },
+  {
+    "revision": "0754657178a6b38c10309b06851d093c",
+    "url": "/psite/static/media/p5.07546571.png"
+  },
+  {
+    "revision": "8d8c98b35ae41b8510ca73b7d1cdc6b2",
+    "url": "/psite/static/media/profile.8d8c98b3.png"
+  }
+]);
